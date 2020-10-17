bb7f8f20fe4f9af156c914d181f135a1ae1274a8
gure-y <y.8kp.gm@gmail.com>
2020-10-17 15:50:47 +0900

    コントローラーにindexと新規登録を追加

--- backend/app/Http/Controllers/ItemsController.php
+++ backend/app/Http/Controllers/ItemsController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class ItemsController extends Controller
 {
-    //
+    public function index(){
+      $items =  Item::latest()->get();
+      return view('index', ['items' => $items]);
+    }
+
+    // createのviewをレンダリング
+    public function create(){
+        return view('create');
+    }
+
+    public function store(Request $request){
+        $inputs = $request->all();
+        Item::create($inputs);
+        return redirect()->route('index');
+    }
 }
