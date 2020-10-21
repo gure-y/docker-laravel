cf4900a6f0b8d0ecb04edfeb32c676c6045ce205
gure-y <y.8kp.gm@gmail.com>
2020-10-21 21:52:20 +0900

    ルーティングを条件分岐

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,26 +12,22 @@ use App\Http\Controllers\ItemsController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 Route::get('/',[ItemsController::class, 'index'])->name('index');
-
-Route::get('/create',[ItemsController::class, 'create'])->name('create');
-Route::post('/store',[ItemsController::class, 'store'])->name('store');
-
-Route::get('/edit/{id}', [ItemsController::class, 'edit'])->name('edit');
-Route::post('/update', [ItemsController::class, 'update'])->name('update');
-
-Route::get('/delete/{id}', [ItemsController::class, 'delete'])->name('delete');
+Auth::routes();
 
 Route::get('/show/{id}', [ItemsController::class, 'show'])->name('show');
 
-Auth::routes(); 
-// ↑は、vendor/laravel/framework/src/Illuminate/Routing/Router.php
-// に記述されているauthメソッドを呼び出している。
-// これではじめから用意されているuser系のルーティングは記述する必要がなくなる(devise forみたいなもの)
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::get('/bookmark', [ItemsController::class, 'bookmark'])->name('bookmark');
 Route::post('/bookmark', [ItemsController::class, 'addBookmark'])->name('addBookmark');
+
+//管理者ユーザーにのみ許可
+Route::prefix('admin')->middleware('can:admin')->group(function() {
+  Route::get('/create',[ItemsController::class, 'create'])->name('create');
+  Route::post('/store',[ItemsController::class, 'store'])->name('store');
+  
+  Route::get('/edit/{id}', [ItemsController::class, 'edit'])->name('edit');
+  Route::post('/update', [ItemsController::class, 'update'])->name('update');
+  
+  Route::get('/delete/{id}', [ItemsController::class, 'delete'])->name('delete');
+  Route::get('/adminpage',[ItemsController::class, 'adminpage'])->name('adminpage');
+});
