f657d2c1089aed7bcdb41bcf7aab2cdb247370ca
gure-y <y.8kp.gm@gmail.com>
2020-10-21 21:51:48 +0900

    userテーブルにis_adminカラムを追加

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin', function (\App\Models\User $user) {
+            return $user->is_admin;
+        });
     }
 }
