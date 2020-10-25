3ef0c84fa3ced5c6f1589c1686aacca55b8af5fd
gure-y <y.8kp.gm@gmail.com>
2020-10-25 20:24:34 +0900

    ALL PRODUCTSページを作成

---
+++ backend/resources/views/list.blade.php
@@ -0,0 +1,31 @@
+@extends('layout')
+@section('title', 'ワンピース一覧')
+@section('content')
+
+<div class="text-black-50 text-center border-bottom mt-5">
+  <h5>ALL PRODUCTS</h5>
+</div>
+
+<div class="container py-4 center-block">
+  <div class="row">
+    @if(!isset($items[0]))
+      <span>登録なし</span>
+    @else
+      @foreach ($items as $item)
+      <div class="col-md-3 text-left mt-4">
+        <div class="card">
+          <a href="{{ route('show', $item->id) }}">
+            <p class="card-text"><small class="text-muted">　{{ $item->bland }}</small></p>
+            <img src="{{ $item->image }}" class="card-img-top">
+          </a>
+          <div class="card-body">
+            <p class="card-text">¥{{ $item->price }}<small class="text-muted"> 税込</small></p>
+          </div>
+        </div>
+      </div>
+      @endforeach
+    @endif
+  </div>
+</div>
+
+@endsection
