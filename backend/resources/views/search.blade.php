ff5c353cf51302d2e83a1b73a96b9c2e1e516046
gure-y <y.8kp.gm@gmail.com>
2020-10-26 17:05:04 +0900

    検索機能つきビューの調整

--- backend/resources/views/search.blade.php
+++ backend/resources/views/search.blade.php
@@ -6,27 +6,35 @@
   <h5>RESULT</h5>
 </div>
 
-<div class="container py-4 center-block">
-  <div class="row">
-    @if(!isset($data[0]))
-      <span>検索結果はありません</span>
-    @else
-      @foreach ($data as $item)
-      <div class="col-md-3 text-left mt-4">
-        <div class="card">
-          <a href="{{ route('show', $item->id) }}">
-            <p class="card-text"><small class="text-muted">　{{ $item->bland }}</small></p>
-            <img src="{{ $item->image }}" class="card-img-top">
-          </a>
-          <div class="card-body">
-            <p class="card-text">¥{{ $item->price }}<small class="text-muted"> 税込</small></p>
+@if(isset($data[0]))
+  <div class="text-black-50 text-center">
+    <p>{{ $data->count() }}件見つかりました</p>
+  </div>
+@endif
+
+@if(!isset($data[0]))
+  <div class="text-black-50 text-center">
+    <p>検索結果はありません</p>
+  </div>
+@else
+  <div class="container py-4">
+    <div class="row">
+        @foreach ($data as $item)
+        <div class="col-md-3 text-left mt-4">
+          <div class="card">
+            <a href="{{ route('show', $item->id) }}">
+              <p class="card-text"><small class="text-muted">　{{ $item->bland }}</small></p>
+              <img src="{{ $item->image }}" class="card-img-top">
+            </a>
+            <div class="card-body">
+              <p class="card-text">¥{{ $item->price }}<small class="text-muted"> 税込</small></p>
+            </div>
           </div>
         </div>
+        @endforeach
       </div>
-      @endforeach
-      {{ $data->appends(request()->input())->render('pagination::bootstrap-4') }}
-    @endif
-  </div>
-</div>
-
+    </div>
+    {{ $data->appends(request()->input())->render('pagination::bootstrap-4') }}
+@endif
+  
 @endsection
