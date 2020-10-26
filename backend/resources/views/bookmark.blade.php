5ef3fd30b6cea3f13a7319d59cef245ec56ae301
gure-y <y.8kp.gm@gmail.com>
2020-10-26 17:27:05 +0900

    bookmarkページのビューの調整

--- backend/resources/views/bookmark.blade.php
+++ backend/resources/views/bookmark.blade.php
@@ -1,29 +1,33 @@
 @extends('layout')
-
+@section('title', 'ブックマーク一覧')
 @section('content')
 <div class="text-black-50 text-center border-bottom mt-5">
   <h5>MY BOOKMARK PRODUCTS</h5>
 </div>
 
+@if(!isset($bookmarks[0]))
+  <div class="text-black-50 text-center">
+    <p>ブックマークした商品はありません</p>
+  </div>
+@endif
+  
 <p class="text-center">{{ $message ?? ''}}</p><br>
-<div class="card-body">
-    @foreach($bookmarks as $bookmark)
-        <div class="col-md-4 text-left mt-4">
-            <div class="card-deck">
-            <div class="img-flex-4">
-                <div class="card">
-                    <a href="{{ route('show', $bookmark->item->id) }}">
-                        <p class="card-text"><small class="text-muted">　{{ $bookmark->item->bland }}</small></p>
-                        <img src="{{ $bookmark->item->image }}" class="card-img-top">
-                    </a>
-                    <div class="card-body">
-                        <p class="card-text">¥{{ $bookmark->item->price }}<small class="text-muted"> 税込</small></p>
-                    </div>
+<div class="container py-4">
+    <div class="row">
+        @foreach($bookmarks as $bookmark)
+        <div class="col-md-3 text-left mt-4">
+            <div class="card">
+                <a href="{{ route('show', $bookmark->item->id) }}">
+                    <p class="card-text"><small class="text-muted">　{{ $bookmark->item->bland }}</small></p>
+                    <img src="{{ $bookmark->item->image }}" class="card-img-top">
+                </a>
+                <div class="card-body">
+                    <p class="card-text">¥{{ $bookmark->item->price }}<small class="text-muted"> (tax incl.)</small></p>
                 </div>
             </div>
-            </div>
         </div>
-    @endforeach
+        @endforeach
+    </div>
 </div>
 
 @endsection
